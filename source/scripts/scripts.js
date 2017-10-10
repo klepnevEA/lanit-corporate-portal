e5906f706775281f711469e44a510f88ae3f9671
Klepnev <klepnev-ea@yandex.ru>
2017-10-10 20:15:22 +0300

    сделал боковое меню

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
 
+	//открытие/зактытие пунктов в боковом меню
+	var openMenu = function(e) {
+		e.preventDefault();
+		var thisEl = $(this),
+			parentEl = thisEl.closest('.header__menu-elem-main');
 
+			parentEl.toggleClass('active');
+	}
+
+	$('.header__menu-link-main').on('click', openMenu);
+
+	// сворачивание бокового меню
+
+	var curtailMenu =  function(e) {
+		e.preventDefault();
+		$('.header__menu').toggleClass('curtail');
+		$('.header__menu-elem-main').removeClass('active');
+	}
+
+	$('.header__turn').on('click', curtailMenu);
 }); 
 
 
