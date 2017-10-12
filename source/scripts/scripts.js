c1719b59d58358b128374d863f08f09747584c7d
Klepnev <klepnev-ea@yandex.ru>
2017-10-12 17:02:29 +0300

    верстка

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -49,6 +49,28 @@ $(document).ready(function() {
 
 	$('.food__tab').on('click', tabOpen);
 
+
+
+	// троеточие в тексте
+	
+	var box = document.querySelector('.advertising__adt-text'),
+    text = box.innerHTML,
+    clone = document.createElement('div');
+    console.log(box);
+
+	clone.style.position = 'absolute';
+	clone.style.visibility = 'hidden';
+	clone.style.width = box.clientWidth + 'px';
+	clone.innerHTML = text;
+	document.body.appendChild(clone);
+
+	var l = text.length - 1;
+	for (; l >= 0 && clone.clientHeight > box.clientHeight; --l) {
+	    clone.innerHTML = text.substring(0, l) + '...';
+	}
+
+	box.innerHTML = clone.innerHTML;
+
 }); 
 
 
