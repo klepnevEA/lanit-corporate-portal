5825196ce3e5aaea2d1afbb92e60f2a26523a721
Klepnev <klepnev-ea@yandex.ru>
2017-10-16 17:36:16 +0300

    верстка

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -78,9 +78,75 @@ Stickyfill.refreshAll();
 
 	box.innerHTML = clone.innerHTML;
 
+	// закрыть любой попап
 
-	
+	$('.popup__close').on('click', function(e) {
+		e.preventDefault();
+		$(this).closest('.popup').removeClass('active');
+	});
 
+
+	// настроение в офисе попап
+
+	$('.mood-office__link').on('click', function(e) {
+		e.preventDefault();
+		var thisEl = $(this),
+			thisVal = thisEl.data('popup');
+		$('#' + thisVal).addClass('active');
+	});
+
+	// круговая  диаграмма
+
+			var dataset = [
+			{
+			    value: 85,
+			    color: '#ff8b00'
+			  }, {
+			    value: 10,
+			    color: '#00aeff'
+			  },  {
+			    value: 5,
+			    color: '#adadad'
+			  }
+			];
+
+			var maxValue = 25;
+			var container = $('.circle');
+
+			var addSector = function(data, startAngle, collapse) {
+			  var sectorDeg = 3.6 * data.value;
+			  var skewDeg = 90 + sectorDeg;
+			  var rotateDeg = startAngle;
+			  if (collapse) {
+			    skewDeg++;
+			  }
+
+			  var sector = $('<div>', {
+			    'class': 'sector'
+			  }).css({
+			    'background': data.color,
+			    'transform': 'rotate(' + rotateDeg + 'deg) skewY(' + skewDeg + 'deg)'
+			  });
+			  container.append(sector);
+
+			  return startAngle + sectorDeg;
+			};
+
+			dataset.reduce(function (prev, curr) {
+			  return (function addPart(data, angle) {
+			    if (data.value <= maxValue) {
+			      return addSector(data, angle, false);
+			    }
+
+			    return addPart({
+			      value: data.value - maxValue,
+			      color: data.color
+			    }, addSector({
+			      value: maxValue,
+			      color: data.color,
+			    }, angle, true));
+			  })(curr, prev);
+			}, 0);
 }); 
 
 
