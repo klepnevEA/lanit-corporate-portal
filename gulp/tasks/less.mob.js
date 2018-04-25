91cc0ec4c4d3fc93b6da045dadb73df0dce666d7
Parker <parkers@mail.ru>
2018-04-25 11:42:15 +0300

    без мапинга

--- gulp/tasks/less.mob.js
+++ gulp/tasks/less.mob.js
@@ -5,7 +5,7 @@ var path = require('path');
 
   $.gulp.task('less:mob', function() {
     return $.gulp.src('./source/css/style.mob.less')
-      .pipe($.gp.sourcemaps.init())
+      //.pipe($.gp.sourcemaps.init())
       .pipe($.gp.less()).on('error', $.gp.notify.onError({ title: 'Style' }))
       .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
       .pipe($.gp.sourcemaps.write())
